5350d9abde81055cc1489b764af0e3d62403f27f
TylerSustare <tyler.sustare@shopify.com>
2020-12-14 21:50:38 -0800

    Refactor day 6

--- src/year2020/day06.rb
+++ src/year2020/day06.rb
@@ -2,20 +2,13 @@ module Year2020
   class Day06
     def part1(input)
       input.reduce(0) do |total, answers|
-        merged_answers = Set[]
-        answers.each { |answer| merged_answers.merge(answer) }
-        total += merged_answers.count
+        total += answers.reduce(Set[]) { |group_answers, answer| group_answers.merge(answer) }.count
       end
     end
 
     def part2(input)
       input.reduce(0) do |total, answers|
-        group_answers = nil
-        answers.each do |answer|
-          group_answers ||= answer
-          group_answers = group_answers.intersection(answer)
-        end
-        total += group_answers.count
+        total += answers.reduce { |group_answers, answer| group_answers = group_answers.intersection(answer) }.count
       end
     end
   end
